fc5904f085f3021097e5333b2506b1eef979e17e
Madhu-R-cloud <madhusudanr00@gmail.com>
2023-12-09 17:16:02 +0530

    Student marks,percentage calculation

--- StudentDetails.js
+++ StudentDetails.js
@@ -1,1 +1,223 @@
-console.log("Student details and marks, percentage calculation")
+// console.log("Student details and marks, percentage calculation")
+
+// - compute test score for 3 subjects
+// - compute total marks 
+// - compute percentage
+// - allocate grade (100-90 = A, 75-90 = B,  = C, 20< = D)
+
+
+const StudentData = [
+    {
+        Roll_no: 501,
+        Name: "Liam Garcia",
+        Class: 5,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 502,
+        Name: "Ava Robinson",
+        Class: 5,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 503,
+        Name: "Lucas Cooper",
+        Class: 5,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 504,
+        Name: "Emma Reed",
+        Class: 5,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 505,
+        Name: "Mia Hughes",
+        Class: 5,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 601,
+        Name: "Sophia Smith",
+        Class: 6,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 602,
+        Name: "Ethan Johnson",
+        Class: 6,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 603,
+        Name: "Ava Williams",
+        Class: 6,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 604,
+        Name: "Noah Brown",
+        Class: 6,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 605,
+        Name: "Olivia Jones",
+        Class: 6,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 701,
+        Name: "Liam Davis",
+        Class: 7,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 702,
+        Name: "Emma Martinez",
+        Class: 7,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 703,
+        Name: "Mia Wilson",
+        Class: 7,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 704,
+        Name: "Lucas Taylor",
+        Class: 7,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 705,
+        Name: "Aiden Anderson",
+        Class: 7,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 801,
+        Name: "Isabella Thomas",
+        Class: 8,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 802,
+        Name: "James White",
+        Class: 8,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 803,
+        Name: "Avery Clark",
+        Class: 8,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 804,
+        Name: "Mason Turner",
+        Class: 8,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 805,
+        Name: "Charlotte Harris",
+        Class: 8,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 801,
+        Name: "Evelyn Scott",
+        Class: 9,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 802,
+        Name: "Logan King",
+        Class: 9,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 803,
+        Name: "Harper Turner",
+        Class: 9,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 804,
+        Name: "Jackson Lee",
+        Class: 9,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 805,
+        Name: "Abigail Baker",
+        Class: 9,
+        Gender: "Female",
+        test_score: []
+    }	
+] 
+
+    // const test_score = [{sub: "maths", marks: 70}];
+
+    function randomValue(min,max){
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+   
+    const randomNum = randomValue(30,90);
+
+    TestScore()
+
+    function TestScore(){
+        // console.log(StudentData[0].test_score)
+      
+        for(let student of StudentData){
+            const mathsMarks = randomValue(30,90);
+            const scienceMarks = randomValue(30,90);
+            const socialScienceMarks = randomValue(30,90);
+           
+            let totalScore = mathsMarks + scienceMarks + socialScienceMarks;
+            let percentage = Math.round(totalScore / 3);
+        
+            let Grade =  percentage>=90 ?  'A' :  percentage>=70 && percentage<=90 ?  'B' 
+            : percentage>=20 && percentage<=69
+             ?  'C' :  'D'
+                    
+             student.test_score.push({sub : 'Mathematics', marks: mathsMarks},{sub : 'Science', marks: scienceMarks},{sub : 'Social Science', marks: socialScienceMarks});
+                student.Totalscore = totalScore;
+                student.Percentage = percentage;
+                student.Grade = Grade;
+               console.log(student);
+
+
+        }
+    
+    }
+
+        
